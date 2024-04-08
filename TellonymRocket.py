8dee4c81b17781ca3460becaa35969ab8ed663d6
Devwithoverthinking <35379048+DevWithOverthinking@users.noreply.github.com>
2024-04-08 14:54:25 +0300

    Add files via upload

---
+++ TellonymRocket.py
@@ -0,0 +1,6 @@
+import requests
+
+# Contact Mission Control 📡
+# Discord : 7w2
+print("Contacting Mission Control Commander on Discord at 7w2...")
+print("Mission Control Commander: Hey there, fellow astronaut! What's the craziest thing you've seen in the galaxy today? Just remember, if you see a space cat, don't try to pet it!")
